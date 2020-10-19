3c9ab9ab396e9a4c58034b7f64c01d981a3eaac4
JammyFish135 <53277832+JammyFish135@users.noreply.github.com>
2020-10-19 18:48:47 -0500

    Add files via upload

---
+++ facts.js
@@ -0,0 +1,263 @@
+var FactsObj = {
+	config:{
+		catHomeTitle: "Facts",
+		catHomeColor: "#00F2F7",
+	},
+
+	//This "0" is the object name of the first CATEGORY for indexing purposes
+	0:{
+		config:{
+			catTitle: "Mythology",
+			catColor: "#F4C13B",
+			catPic: "",
+		},
+
+		//This "0" is the object name of the first SUB-CATEGORY for indexing purposes
+		0: {
+			config:{
+				subCatTitle: "Greek",
+				subCatColor: "#CE4629",
+				index: 0,
+			},
+
+			//This "0" is the object name of the first SLIDE for indexing purposes
+			0: {
+				text: "Prometheus was the youngest of the Titans. His pity for humanity caused him to steal the secret knowledge of fire from Mount Olympus.",
+				pic: "414px-Heinrich_fueger_1817_prometheus_brings_fire_to_mankind.jpg",
+			},
+
+			//second slide object
+			1: {
+				text: "Poseidon caused King Minos' wife Pasiphae to fall in love with a pure white bull. She would become the mother of the dreadful Minotaur.",
+				pic: "minotaur-1484121852-1552244563350.jpg",
+			},
+
+			//third slide object . . .
+			2: {
+				text: "Dionysus was the god of wine and ritual madness. His thyrsus, wrapped with ivy and dripping with honey, was both a beneficent wand and a sinister weapon.",
+				pic: "dionysus_33.jpg",
+			},
+
+			3: {
+				text: "\"... (the) Chimaera who breathed raging fire, a creature fearful, ... swift-footed and strong, who had three heads, one of a grim-eyed lion; in her hinderpart, a dragon; and in her middle, a goat, breathing forth a fearful blast of blazing fire.\" (Homer's Iliad)",
+				pic: "7y2M1yY.png",
+			},
+		},
+
+		//This "1" is the object name of the second SUB-CATEGORY for indexing purposes
+		1: {
+			config:{
+				subCatTitle: "Norse",
+				subCatColor: "#417659",
+				index: 0,
+			},
+
+			//This "0" is the object name of the first SLIDE for indexing purposes
+			0: {
+				text: "Loki asked a giant to help build Asgard, one of the nine worlds of the gods. The giant asked for the sun and moon if he completed the work on time... Loki made sure that the giant did not finish the work on time by leading the giant’s horse away.",
+				pic: "I-am-the-Giant-Skrymir.jpg",
+			},
+
+			//second slide object
+			1: {
+				text: "The Mid-Guard Serpent Jörmungandr was large enough to wrap around the Earth and grasp its tail in its mouth. It was said that when he releases his tail, the apocalypse of Ragnarok will begin.",
+				pic: "luca-lonardi-7thor.jpg",
+			},
+
+			//third slide object . . .
+			2: {
+				text: "Norse Slide 2",
+				pic: "AM_738_4to_Ratatoskr.png",
+			},
+
+			3: {
+				text: "Norse Slide 3",
+				pic: "",
+			},
+		},
+	},
+
+	//"1" is the object name of the second CATEGORY
+	1:{
+		config:{
+			catTitle: "Outer Space",
+			catColor: "#8E3068",
+		},
+
+		//This "0" is the object name of the first SUB-CATEGORY for indexing purposes
+		0: {
+			config:{
+				subCatTitle: "Stars & Planets",
+				subCatColor: "#D66A41",
+				index: 0,
+			},
+
+			//"0" is the object name of the first SLIDE for indexing purposes
+			0: {
+				text: "A black hole is super scary. Even light cannot escape its gravitational clutches!",
+				pic: "nasa-black-hole-1280x720.jpg",
+			},
+
+			//second slide object
+			1: {
+				text: "slide 2",
+				pic: "",
+			},
+
+			//third slide object . . .
+			2: {
+				text: "slide 3",
+				pic: "",
+			},
+
+			3: {
+				text: "",
+				pic: "",
+			},
+		},
+
+		//This is the object name of the SUB-CATEGORY for indexing purposes
+		1: {
+			config:{
+				subCatTitle: "Space Travel",
+				subCatColor: "#273C88",
+				index: 0,
+			},
+
+			//"0" is the object name of the first SLIDE for indexing purposes
+			0: {
+				text: "Space ships are neat.",
+				pic: "",
+			},
+
+			//second slide object
+			1: {
+				text: "Space Travel 1",
+				pic: "",
+			},
+
+			//third slide object . . .
+			2: {
+				text: "Space Travel 2",
+				pic: "",
+			},
+
+			3: {
+				text: "Space Travel 3",
+				pic: "",
+			},
+		},
+	},
+
+	//This is the object name of the CATEGORY
+	2:{
+		config:{
+			catTitle: "Creatures",
+			catColor: "#9EC221",
+		},
+
+		//This is the object name of a SUB-CATEGORY for indexing purposes
+		0: {
+			config:{
+				subCatTitle: "Extreme Abilities",
+				subCatColor: "#EC1F0F",
+				index: 0,
+			},
+
+			//"0" is the object name of the first SLIDE for indexing purposes
+			0: {
+				text: "slide 0",
+				pic: "",
+			},
+
+			//second slide object
+			1: {
+				text: "slide 1",
+				pic: "",
+			},
+
+			//third slide object . . .
+			2: {
+				text: "slide 2",
+				pic: "",
+			},
+
+			3: {
+				text: "slide 3",
+				pic: "",
+			},
+		},
+
+		//This is the object name of the SUB-CATEGORY for indexing purposes
+		1: {
+			config:{
+				subCatTitle: "Dinosaurs",
+				subCatColor: "#4EC7A6",
+				index: 0,
+			},
+
+			//"0" is the object name of the first SLIDE for indexing purposes
+			0: {
+				text: "Sarcosuchus (\"flesh-crocodile\") was a giant relative of crocodiles, with fully grown individuals estimated to have reached up to 9 m (30 ft) in total length and 3.9 tons (7,800 lbs) in weight.",
+				pic: "eldar-zakirov-06-sarcosuchus-closeup1.jpg",
+			},
+
+			//second slide object
+			1: {
+				text: "Scientists now think that ligaments running from the hip to the back of the neck would have allowed Diplodocus (\"Double Beam\") to hold its neck in a horizontal position without using muscles. The vertebrae (back bones) are split down the middle and this space could have held ligaments like these." ,
+				pic: "diplodocus--dinosaurs--sauropod--jurassic.jpg",
+			},
+
+			//third slide object . . .
+			2: {
+				text: "The Velociraptor (\"Swift Seizer\") was a Dromaeosaurid, among the dinosaurs with the very highest I.Q. level, so they were truly smart among dinosaurs. On this ranking, they were probably a bit smarter than rabbits and not quite as smart as cats and dogs.",
+				pic: "beer-1008342_e523c_hd.jpg",
+			},
+
+			3: {
+				text: "Allosaurus (\"Different Lizard\") was named for it's strange vertebrae. Specifically, some of the vertebrae of Allosaurus fragilis (Latin for \"fragile\") were concave on both sides and contained shallow cavities, giving them an hourglass shape — these features reduced the bones' strength, while making them lighter.",
+				pic: "allosaurus-dinosaur_u-l-pzk0gz0.jpg",
+			},
+		},
+	},
+
+	//This is the object name of the CATEGORY
+	3:{
+		config:{
+			catTitle: "Freaky",
+			catColor: "#5AED6B",
+		},
+
+		//This is the object name of a SUB-CATEGORY for indexing purposes
+		0: {
+			config:{
+				subCatTitle: "Strange Deaths",
+				subCatColor: "#860303",
+				index: 0,
+			},
+
+			//"0" is the object name of the first SLIDE for indexing purposes
+			0: {
+				text: "slide 0",
+				pic: "",
+			},
+
+			//second slide object
+			1: {
+				text: "slide 1",
+				pic: "",
+			},
+
+			//third slide object . . .
+			2: {
+				text: "slide 2",
+				pic: "",
+			},
+
+			3: {
+				text: "slide 3",
+				pic: "",
+			},
+		},
+	},
+};
